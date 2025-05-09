bf9e371991e5bb24a7580fb611ec8c04b19d6543
Patricio Pantaleo <74371185+Ppantaleo@users.noreply.github.com>
2025-05-09 11:40:47 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @defgroup plugins_blocks_supportBlock Support Block Plugin
+ */
+
+/**
+ * @file plugins/blocks/supportBlock/index.php
+ *
+ * Copyright (c) 2025 Paideia Studio
+ * Distributed under the GNU GPL v3.
+ *
+ * @ingroup plugins_blocks_supportBlock
+ * @brief Wrapper para el plugin de bloque de soporte.
+ *
+ */
+
+require_once('SupportBlockPlugin.inc.php');
+
+return new SupportBlockPlugin();
+
+?>
